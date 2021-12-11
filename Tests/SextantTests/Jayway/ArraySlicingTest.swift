6fb373293771b12587432bc5b2ff080af9059800
Rocco Bowling <rocco@chimerasw.com>
2021-12-11 01:02:25 -0500

    Adding ArraySlicingTest

---
+++ Tests/SextantTests/Jayway/ArraySlicingTest.swift
@@ -0,0 +1,45 @@
+import XCTest
+import Hitch
+import class Foundation.Bundle
+
+@testable import Sextant
+
+class ArraySlicingTest: TestsBase {
+    let jsonArray = "[1, 3, 5, 7, 8, 13, 20]"
+    
+    func test_get_by_position() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[3]"), [7])
+    }
+    
+    func test_get_from_index() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[:3]"), [ 1, 3, 5 ])
+    }
+
+    func test_get_between_index() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[1:5]"), [ 3, 5, 7, 8 ])
+    }
+
+    func test_get_between_index_2() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[0:1]"), [ 1 ])
+    }
+
+    func test_get_between_index_3() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[0:2]"), [ 1, 3 ])
+    }
+
+    func test_get_between_index_out_of_bounds() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[1:15]"), [ 3, 5, 7, 8, 13, 20 ])
+    }
+
+    func test_get_from_tail_index() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[-3:]"), [ 8, 13, 20 ])
+    }
+
+    func test_get_from_tail() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[3:]"), [ 7, 8, 13, 20 ])
+    }
+
+    func test_get_indexes() {
+        XCTAssertEqualAny(jsonArray.query(values: "$[0,1,2]"), [ 1, 3, 5 ])
+    }
+}
